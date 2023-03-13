49c79e3f50069baf6dfe93c0a37788c3eaa00ab3
Lexsa Campbell <lexsacampbell@gmail.com>
2023-03-13 13:24:50 -0700

    Removed isFacingUp default value

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import backOfCard from "./back.png";
 import "./PlayingCard.css";
 import useFlip from './hooks/useFlip';
 
 /* Renders a single playing card. */
 function PlayingCard({ front, back = backOfCard }) {
-  const [isFacingUp, setIsFacingUp] = useFlip(true);
+  const [isFacingUp, setIsFacingUp] = useFlip();
 
   return (
     <img
